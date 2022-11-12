328339c68007c59472265b68a018980ddf98d314
Darren Jaworski <darrenjaworski@gmail.com>
2022-11-12 16:26:44 -0600

    performer and teacher

--- src/presentation/components/molecules/hero.tsx
+++ src/presentation/components/molecules/hero.tsx
@@ -1,9 +1,11 @@
 export const Hero = () => {
   return (
+    // TODO update the bg image on a rotation
+    // TODO add photo credits
     <section className="julianne_welcome">
       <div className="hero_text">
         <h1 data-testid="hero-text">Julianne Reynolds</h1>
-        <h3>Soprano</h3>
+        <h3>Performer & Teacher</h3>
       </div>
     </section>
   );
