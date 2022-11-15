6f047841bb3c984b3aa841c32112db2b00a84b2b
Darren Jaworski <darrenjaworski@gmail.com>
2022-11-14 21:02:17 -0600

    tweak some text

--- src/presentation/components/atoms/carousel.tsx
+++ src/presentation/components/atoms/carousel.tsx
@@ -10,108 +10,104 @@ import yes from "../../imgs/performances/yes.jpg";
 import { PerformanceBlock, PerformanceText } from "./performanceBlock";
 
 const PositionedSlider = styled(Slider)`
-  width: calc(100% - 2rem);
-  padding: 2rem 0;
+    width: calc(100% - 2rem);
+    padding: 2rem 0;
 `;
 
 export enum SlideMode {
-  light,
-  dark,
+    light,
+    dark,
 }
 
 export interface Performance {
-  heading: string;
-  description: string | JSX.Element;
-  date: string;
-  img: string;
-  mode: SlideMode;
+    heading: string;
+    description: string | JSX.Element;
+    date: string;
+    img: string;
+    mode: SlideMode;
 }
 
 const performances: Performance[] = [
-  {
-    heading: "Christmas Choral Concert",
-    description:
-      "Soloist for Christmas Choral Concert at St. Luke’s United Methodist Church of OKC",
-    date: "December 7, 2022",
-    img: christmas,
-    mode: SlideMode.dark,
-  },
-  {
-    heading: "Female Swing",
-    description:
-      "Oklahoma! Interactive. Lyric Theatre of Oklahoma Educational Outreach tour.",
-    date: "2022 through 2023",
-    img: oklahoma,
-    mode: SlideMode.dark,
-  },
-  {
-    heading: "Ashley the Bride",
-    description: (
-      <>
-        <PerformanceText mode={SlideMode.dark}>
-          YES! THE MUSICAL in Concert at Ponyboy OKC
-        </PerformanceText>
-        <PerformanceText mode={SlideMode.dark}>
-          Directed by Shea Sullivan
-        </PerformanceText>
-        <PerformanceText mode={SlideMode.dark}>
-          Music Directed by Drew Wutke
-        </PerformanceText>
-        <PerformanceText mode={SlideMode.dark}>
-          Written by Blaine Hopkins and Garrett Kotecki
-        </PerformanceText>
-      </>
-    ),
-    date: "November 6th, 2022",
-    img: yes,
-    mode: SlideMode.dark,
-  },
-  {
-    heading: "Julie Jordan",
-    description: "Carousel. Lyric Theatre of Oklahoma",
-    date: "July 2022",
-    img: carousel,
-    mode: SlideMode.light,
-  },
-  {
-    heading: "Sharon Graham",
-    description: "Masterclass. Lyric Theatre of Oklahoma",
-    date: "September 2021",
-    img: masterclass,
-    mode: SlideMode.dark,
-  },
+    {
+        heading: "Christmas Choral Concert",
+        description:
+            "Soloist for Christmas Choral Concert at St. Luke’s United Methodist Church of OKC",
+        date: "December 7, 2022",
+        img: christmas,
+        mode: SlideMode.dark,
+    },
+    {
+        heading: "Oklahoma! Interactive",
+        description:
+            "Female Swing. Lyric Theatre of Oklahoma Educational Outreach tour.",
+        date: "2022 through 2023",
+        img: oklahoma,
+        mode: SlideMode.dark,
+    },
+    {
+        heading: "YES! THE MUSICAL In Concert",
+        description: (
+            <>
+                <PerformanceText mode={SlideMode.dark}>
+                    Ashley, The Bride
+                </PerformanceText>
+                <PerformanceText mode={SlideMode.dark}>
+                    Oklahoma City, OK
+                </PerformanceText>
+            </>
+        ),
+        date: "November 6th, 2022",
+        img: yes,
+        mode: SlideMode.dark,
+    },
+    {
+        heading: "Carousel",
+        description: "Julie Jordan. Lyric Theatre of Oklahoma",
+        date: "July 2022",
+        img: carousel,
+        mode: SlideMode.light,
+    },
+    {
+        heading: "Masterclass",
+        description: "Sharon Graham. Lyric Theatre of Oklahoma",
+        date: "September 2021",
+        img: masterclass,
+        mode: SlideMode.dark,
+    },
 ];
 
 export const Carousel = () => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    slidesToShow: 4,
-    responsive: [
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 850,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
+    const settings = {
+        dots: false,
+        infinite: false,
+        slidesToShow: 4,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
 
-  const PerformanceBlocks = performances.map((performance, i) => {
-    return <PerformanceBlock key="i" {...performance}></PerformanceBlock>;
-  });
+    const PerformanceBlocks = performances.map((performance, i) => {
+        return <PerformanceBlock key="i" {...performance}></PerformanceBlock>;
+    });
 
-  return <PositionedSlider {...settings}>{PerformanceBlocks}</PositionedSlider>;
+    return (
+        <PositionedSlider {...settings}>{PerformanceBlocks}</PositionedSlider>
+    );
 };
--- src/presentation/components/atoms/performanceBlock.tsx
+++ src/presentation/components/atoms/performanceBlock.tsx
@@ -3,60 +3,61 @@ import React from "react";
 import { Performance, SlideMode } from "./carousel";
 
 const StyledPerformanceContainer = styled.div<{ img: string }>`
-  height: 500px;
-  padding: 0.5rem;
-  position: relative;
+    height: 500px;
+    padding: 0.5rem;
+    position: relative;
 `;
 
 const ImageBackground = styled.div<{ img: string }>`
-  height: calc(500px - 1rem);
-  background: #f5f4ca url(${(props) => props.img}) no-repeat center center;
-  background-size: cover;
-  filter: blur(4px);
+    height: calc(500px - 1rem);
+    background: #f5f4ca url(${(props) => props.img}) no-repeat center center;
+    background-size: cover;
+    filter: blur(4px);
 `;
 
 const TextContainer = styled.div`
-  height: calc(500px - 2rem);
-  margin: 1rem;
-  padding: 1rem;
-  position: absolute;
-  top: 0;
-  left: 0;
+    height: calc(500px - 2rem);
+    margin: 1rem;
+    padding: 1rem;
+    position: absolute;
+    top: 0;
+    left: 0;
 `;
 
 const PerformanceHeading = styled.h4<{ mode: SlideMode }>`
-  margin-top: 0;
-  font-size: 1.75rem;
-  letter-spacing: 2px;
-  color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
+    margin-top: 0;
+    font-size: 1.75rem;
+    letter-spacing: 2px;
+    font-style: italic;
+    color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
 `;
 
 const PerformanceDate = styled.p<{ mode: SlideMode }>`
-  position: absolute;
-  bottom: 0;
-  color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
+    position: absolute;
+    bottom: 0;
+    color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
 `;
 
 export const PerformanceText = styled.p<{ mode: SlideMode }>`
-  color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
+    color: ${(props) => (props.mode === SlideMode.dark ? "white" : "black")};
 `;
 
 interface PerformanceBlockProps extends Performance {}
 
 export const PerformanceBlock = (props: PerformanceBlockProps) => {
-  const { heading, description, date, img, mode } = props;  
-  return (
-    <StyledPerformanceContainer img={img}>
-      <ImageBackground img={img} />
-      <TextContainer>
-        <PerformanceHeading mode={mode}>{heading}</PerformanceHeading>
-        {React.isValidElement(description) ? (
-          description
-        ) : (
-          <PerformanceText mode={mode}>{description}</PerformanceText>
-        )}
-        <PerformanceDate mode={mode}>{date}</PerformanceDate>
-      </TextContainer>
-    </StyledPerformanceContainer>
-  );
+    const { heading, description, date, img, mode } = props;
+    return (
+        <StyledPerformanceContainer img={img}>
+            <ImageBackground img={img} />
+            <TextContainer>
+                <PerformanceHeading mode={mode}>{heading}</PerformanceHeading>
+                {React.isValidElement(description) ? (
+                    description
+                ) : (
+                    <PerformanceText mode={mode}>{description}</PerformanceText>
+                )}
+                <PerformanceDate mode={mode}>{date}</PerformanceDate>
+            </TextContainer>
+        </StyledPerformanceContainer>
+    );
 };
