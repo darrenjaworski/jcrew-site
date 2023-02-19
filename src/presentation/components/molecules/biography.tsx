42615084594396836211985e67cf22a5e85e5d4f
Darren Jaworski <darrenjaworski@gmail.com>
2023-02-19 11:53:45 -0600

    feat: add resume, bullet point on bio

--- src/presentation/components/molecules/biography.tsx
+++ src/presentation/components/molecules/biography.tsx
@@ -1,58 +1,53 @@
 import styled from "@emotion/styled";
 
 const BiographyContainer = styled.div`
-    width: 50%;
-    max-width: 50%;
-    display: inline-block;
-    padding: 3rem;
-    background-color: ${(props) => {
-        // @ts-ignore
-        return props.theme.colors.secondary;
-    }};
-    @media (max-width: 750px) {
-        width: 100%;
-        max-width: 100%;
-        padding: 1rem;
-    }
+  width: 50%;
+  max-width: 50%;
+  display: inline-block;
+  padding: 3rem;
+  background-color: ${(props) => {
+    // @ts-ignore
+    return props.theme.colors.secondary;
+  }};
+  @media (max-width: 750px) {
+    width: 100%;
+    max-width: 100%;
+    padding: 1rem;
+  }
 `;
 
 export const Biography = () => {
-    return (
-        <section
-            className="julianne_biography container row flip-mobile"
-            id="biography"
-        >
-            <BiographyContainer>
-                <p data-testid="bio-text">
-                    I’m Julianne Reynolds, an Oklahoma City-based singer, actor
-                    and voice teacher. I am passionate about performing across
-                    styles and have experience in plays, operas, musical
-                    theater, choral singing, and theater for young audiences. I
-                    am happiest when I’m collaborating with other artists on and
-                    off stage, in the rehearsal room, or in the voice studio.
-                </p>
-                <p>
-                    I am always looking for new opportunities to connect and
-                    grow as an artist. Reach out to me if you’d like to
-                    collaborate! I am currently available to work in the
-                    following areas:
-                </p>
-                <ul>
-                    <li>
-                        Performing live in musicals, operas, plays and concerts
-                    </li>
-                    <li>Acting on camera</li>
-                    <li>
-                        Opportunities to assistant direct stage musicals or
-                        plays
-                    </li>
-                    <li>Voice acting and voice over work</li>
-                    <li>Teaching voice lessons virtually or in person</li>
-                </ul>
-            </BiographyContainer>
-            <div className="split container row justify-centered align-items-center">
-                <h2 data-testid="bio-title">Biography</h2>
-            </div>
-        </section>
-    );
+  return (
+    <section
+      className="julianne_biography container row flip-mobile"
+      id="biography"
+    >
+      <BiographyContainer>
+        <p data-testid="bio-text">
+          I’m Julianne Reynolds, an Oklahoma City-based singer, actor and voice
+          teacher. I am passionate about performing across styles and have
+          experience in plays, operas, musical theater, choral singing, and
+          theater for young audiences. I am happiest when I’m collaborating with
+          other artists on and off stage, in the rehearsal room, or in the voice
+          studio.
+        </p>
+        <p>
+          I am always looking for new opportunities to connect and grow as an
+          artist. Reach out to me if you’d like to collaborate! I am currently
+          available to work in the following areas:
+        </p>
+        <ul>
+          <li>Performing live in musicals, operas, plays and concerts</li>
+          <li>Acting on camera</li>
+          <li>Opportunities to assistant direct stage musicals or plays</li>
+          <li>Voice acting and voice over work</li>
+          <li>Teaching voice lessons virtually or in person</li>
+          <li>Workshopping and performance new works</li>
+        </ul>
+      </BiographyContainer>
+      <div className="split container row justify-centered align-items-center">
+        <h2 data-testid="bio-title">Biography</h2>
+      </div>
+    </section>
+  );
 };
--- src/presentation/components/molecules/contact.tsx
+++ src/presentation/components/molecules/contact.tsx
@@ -1,54 +1,66 @@
 import styled from "@emotion/styled";
 
 const ContactContainer = styled.div`
-    width: 50%;
-    max-width: 50%;
-    display: inline-block;
-    display: flex;
-    background-color: ${(props) => {
-        // @ts-ignore
-        return props.theme.colors.secondary;
-    }};
-    flex-direction: column;
-    padding: 3rem;
-    @media (max-width: 750px) {
-        width: 100%;
-        max-width: 100%;
-        padding: 1rem;
-    }
+  width: 50%;
+  max-width: 50%;
+  display: inline-block;
+  display: flex;
+  background-color: ${(props) => {
+    // @ts-ignore
+    return props.theme.colors.secondary;
+  }};
+  flex-direction: column;
+  padding: 3rem;
+  @media (max-width: 750px) {
+    width: 100%;
+    max-width: 100%;
+    padding: 1rem;
+  }
 `;
 
 export const Contact = () => {
-    return (
-        <section className="julianne_contact container row" id="contact">
-            <div className="split container row justify-centered align-items-center">
-                <h2>Contact</h2>
-            </div>
-            <ContactContainer>
-                <div className="contact_block">
-                    <h3>Reach Me</h3>
-                    <p>
-                        <a
-                            href="mailto:julianne.e.reynolds@gmail.com"
-                            data-testid="contact-email"
-                        >
-                            julianne.e.reynolds@gmail.com
-                        </a>
-                    </p>
-                </div>
-                <div className="contact_block">
-                    <h3>Voice Lessons</h3>
-                    <p>
-                        I am available to teach private lessons in person or
-                        virtually, and am a proud affiliate of Kismet Arts
-                        Collective OKC
-                    </p>
-                    <p>
-                        I have experience teaching students of all ages and
-                        across multiple styles of singing.
-                    </p>
-                </div>
-            </ContactContainer>
-        </section>
-    );
+  return (
+    <section className="julianne_contact container row" id="contact">
+      <div className="split container row justify-centered align-items-center">
+        <h2>Contact</h2>
+      </div>
+      <ContactContainer>
+        <div className="contact_block">
+          <h3>Performance Resume</h3>
+          <p>
+            <a
+              href="https://drive.google.com/file/d/1LK7qLjydexOK7Q2PtXc1nL1cg8gJfNui/view?usp=sharing"
+              data-testid="contact-resume"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Download Performance Resume
+            </a>
+          </p>
+        </div>
+        <div className="contact_block">
+          <h3>Reach Me</h3>
+          <p>
+            <a
+              href="mailto:julianne.e.reynolds@gmail.com"
+              data-testid="contact-email"
+            >
+              julianne.e.reynolds@gmail.com
+            </a>
+          </p>
+        </div>
+        <div className="contact_block">
+          <h3>Voice Lessons</h3>
+          <p>
+            I am available to teach private lessons in person or virtually, and
+            am a proud affiliate of Kismet Arts Collective OKC
+          </p>
+          <p>
+            I have experience teaching students of all ages and across multiple
+            styles of singing.
+          </p>
+        </div>
+      </ContactContainer>
+    </section>
+  );
 };
